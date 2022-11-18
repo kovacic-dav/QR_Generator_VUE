129d66095c0afdef86a05ca13c6653b6425ff533
kovacic-dav <72748359+kovacic-dav@users.noreply.github.com>
2022-11-18 10:15:01 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# QR_Generator_VUE
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vue-efuycb)
---
+++ src/components/Header.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>Hallo</h1>
+</template>
+
+<script>
+  export default {
+    name: 'App',
+    components: {},
+  };
+</script>
+
+<style></style>
