12b4c6f6d1ab890eb57c1917a5057f07d982a4d9
kovacic-dav <72748359+kovacic-dav@users.noreply.github.com>
2022-11-18 10:31:51 +0100

    Header fast fertig

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,37 @@
 <template>
-  <h1>Hallo</h1>
+  <div class="header">
+    <div class="overheading">
+      <img
+        src="https://www.htl-wels.at/wp-content/uploads/2021/09/htl_logo-768x785.png"
+        alt=""
+      />
+      <div>QR-Code Generator</div>
+    </div>
+    <h3>The easiest way to create QR Code</h3>
+    <div class="subheading">Generate a QR code and download it!</div>
+  </div>
 </template>
 
 <script>
-  export default {
-    name: 'App',
-    components: {},
-  };
+export default {
+  name: 'App',
+  components: {},
+};
 </script>
 
-<style></style>
+<style scoped>
+.header {
+  text-align: center;
+}
+h3 {
+  margin-bottom: 5px;
+}
+.subheading {
+  font-size: 14px;
+}
+img {
+  width: 35px;
+}
+.overheading {
+}
+</style>
